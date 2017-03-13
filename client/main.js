4f920831ec42109bc0f44aa8c9aa45cc0a33c813
xfry <fry.andrade@gmail.com>
2017-03-13 17:21:18 -0500

    render new Layout component

--- client/main.js
+++ client/main.js
@@ -1,10 +1,10 @@
 import React      from "react";
 import ReactDOM   from "react-dom";
-import App        from "./components/App";
+import Layout        from "./components/Layout";
 
 console.log('hola mundo');
 
 ReactDOM.render(
-  <App/>,
+  <Layout />,
   document.getElementById('app')
 );
