6edb7f332b0cec48516d4522db3088217eea12c3
xfry <fry.andrade@gmail.com>
2017-03-13 17:20:37 -0500

    adding new Layout file

---
+++ client/components/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the main layout</h1>
+      </div>
+    );
+  }
+}
+
+export default Layout
